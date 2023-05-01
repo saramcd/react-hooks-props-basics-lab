ade914589ade154df9d3b887604b82f9e304b604
Sara McDonnell <saramcdonnell32@gmail.com>
2023-05-01 16:16:00 -0400

    lab

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,12 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} color={user.color} city={user.city} />
+      <About
+        bio={user.bio}
+        github={user.links.github}
+        linkedin={user.links.linkedin}
+      />
     </div>
   );
 }
